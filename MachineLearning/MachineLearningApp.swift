dbdecd17db31b96a73e9017cbc98a75ef667b3f4
abdWasiq <abdul.wasiq@naheed.pk>
2022-08-01 12:04:20 +0500

    project initialised

---
+++ MachineLearning/MachineLearningApp.swift
@@ -0,0 +1,17 @@
+//
+//  MachineLearningApp.swift
+//  MachineLearning
+//
+//  Created by NaheedPK on 01/08/2022.
+//
+
+import SwiftUI
+
+@main
+struct MachineLearningApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
